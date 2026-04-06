c95735bbc604fc59688372276ee6f022ffe81ef4
drewhy98 <150239451+drewhy98@users.noreply.github.com>
2026-04-06 19:15:07 +0100

    Create proxy-details.php

---
+++ site/proxy-details.php
@@ -0,0 +1,21 @@
+<?php include "includes/header.php"; ?>
+
+<form action="dashboard.php" method="POST">
+
+<h1>Enter the patient's NHS number</h1>
+
+<p class="hint">
+This is a 10 digit number like 999 123 4567.
+</p>
+
+<input type="text" name="nhs_number" required>
+
+<h2>Your relationship to them</h2>
+
+<input type="text" name="relationship">
+
+<button type="submit">Continue</button>
+
+</form>
+
+<?php include "includes/footer.php"; ?>
