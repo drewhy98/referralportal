3369317024f18a73b6b2ef3e93df6c4bb0e55396
drewhy98 <150239451+drewhy98@users.noreply.github.com>
2026-04-06 19:11:11 +0100

    Create header.php

---
+++ site/includes/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>NHS Referral Tracking</title>
+
+<link rel="stylesheet" href="css/styles.css">
+
+</head>
+
+<body>
+
+<header class="header">
+<div class="header-inner">
+
+<img 
+src="https://cdn-lhr.brandkit.com/accounts/165/files/509Y0S1A8B9NJB96WVVZFXTWE9/179628-preview.webp?v=63866260163"
+alt="NHS Wales"
+class="logo">
+
+<span class="service-name">
+Referral Tracking Service
+</span>
+
+</div>
+</header>
+
+<main class="container">
