8d4b5cd577208ce784e586b19a93f12913f1321d
drewhy98 <150239451+drewhy98@users.noreply.github.com>
2026-04-06 19:09:58 +0100

    Create index.php

---
+++ site/index.php
@@ -0,0 +1,23 @@
+<?php include "includes/header.php"; ?>
+
+<h1>Check your hospital referral status</h1>
+
+<div class="panel">
+
+<p>Use this service to check the progress of a referral made by your GP.</p>
+
+<ul>
+<li>see whether your referral has been received</li>
+<li>check if an appointment has been scheduled</li>
+<li>view the current status of your referral</li>
+</ul>
+
+<h2>Before you start</h2>
+
+<p>You will need your NHS number.</p>
+
+<a class="button" href="who-is-this-for.php">Start now</a>
+
+</div>
+
+<?php include "includes/footer.php"; ?>
