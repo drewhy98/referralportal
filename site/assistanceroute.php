5568cf06594437f450ea18155a6a4b4aff93c217
drewhy98 <150239451+drewhy98@users.noreply.github.com>
2026-04-07 15:16:37 +0100

    Update assistanceroute.php

--- site/assistanceroute.php
+++ site/assistanceroute.php
@@ -1,20 +1,15 @@
 <?php
-// Make sure the form was submitted
-if (isset($_POST['assistance_type'])) {
-    $type = $_POST['assistance_type'];
+session_start();
 
-    if ($type === "yes") {
-        header("Location: guidedhelp.php");
-        exit(); // stop execution immediately
-    }
+$type = $_POST['assistance_type'];
 
-    if ($type === "no") {
-        header("Location: dashboard.php");
-        exit();
-    }
-} else {
-    // No value posted; go back to form
-    header("Location: assistancechecker.php");
+if($type == "yes"){
+    header("Location: guidedhelp.php");
+    exit();
+}
+
+if($type == "no"){
+    header("Location: dashboard.php");
     exit();
 }
 ?>
