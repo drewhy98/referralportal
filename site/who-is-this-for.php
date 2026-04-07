ce145181e45c613a4d9b28af88b00c739d1fa679
drewhy98 <150239451+drewhy98@users.noreply.github.com>
2026-04-07 11:29:47 +0100

    Update who-is-this-for.php

--- site/who-is-this-for.php
+++ site/who-is-this-for.php
@@ -1,27 +1,31 @@
 <?php include "includes/header.php"; ?>
 
-<form action="route.php" method="POST">
+<div class="panel">
 
-<h1>Who are you using this service for?</h1>
+  <form action="route.php" method="POST">
 
-<p>Select one option.</p>
+    <h1>Who are you using this service for?</h1>
 
-<div class="radio-option">
-<label>
-<input type="radio" name="user_type" value="self" required>
-Myself
-</label>
+    <p>Select one option.</p>
+
+    <div class="radio-option">
+      <label>
+        <input type="radio" name="user_type" value="self" required>
+        Myself
+      </label>
+    </div>
+
+    <div class="radio-option">
+      <label>
+        <input type="radio" name="user_type" value="proxy">
+        Someone else (for example a family member or person you care for)
+      </label>
+    </div>
+
+    <button type="submit">Continue</button>
+
+  </form>
+
 </div>
-
-<div class="radio-option">
-<label>
-<input type="radio" name="user_type" value="proxy">
-Someone else (for example a family member or person you care for)
-</label>
-</div>
-
-<button type="submit">Continue</button>
-
-</form>
 
 <?php include "includes/footer.php"; ?>
