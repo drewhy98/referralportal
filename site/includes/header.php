3cc9db1081e221c6e2091dc57c416fb0b362a13d
drewhy98 <150239451+drewhy98@users.noreply.github.com>
2026-04-07 16:12:20 +0100

    Update header.php

--- site/includes/header.php
+++ site/includes/header.php
@@ -13,17 +13,37 @@
 <body>
 
 <header class="header">
-<div class="header-inner">
+    <div class="header-inner">
 
-<img 
-src="assets/images/nhslogo.webp"
-alt="NHS Wales"
-class="logo">
+        <img 
+        src="assets/images/nhslogo.webp"
+        alt="NHS Wales"
+        class="logo">
 
-<h1 class="service-name">NHS Wales Referral Tracking Portal</h1>
-</span>
+        <h1 class="service-name">NHS Wales Referral Tracking Portal</h1>
 
-</div>
+    </div>
 </header>
 
 <main class="container">
+
+<?php
+// Breadcrumb system: check if $breadcrumbs is set
+if (!isset($breadcrumbs)) $breadcrumbs = [];
+
+if (!empty($breadcrumbs)): ?>
+<nav aria-label="breadcrumb" class="breadcrumb-container" style="margin: 15px 0;">
+    <ol class="breadcrumb">
+        <?php
+        $last_index = count($breadcrumbs) - 1;
+        foreach ($breadcrumbs as $i => $crumb):
+            if ($i == $last_index): ?>
+                <li class="breadcrumb-item active" aria-current="page"><?= htmlspecialchars($crumb['title']); ?></li>
+            <?php else: ?>
+                <li class="breadcrumb-item"><a href="<?= htmlspecialchars($crumb['url']); ?>"><?= htmlspecialchars($crumb['title']); ?></a></li>
+            <?php endif;
+        endforeach;
+        ?>
+    </ol>
+</nav>
+<?php endif; ?>
